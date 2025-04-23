ed5d471350eb91d293fb0a274d4d8aa1e0fcf0e4
Kenta Urakami <ken.atnek@gmail.com>
2025-04-23 20:57:50 +0900

    📱 レスポンシブ調整

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -9,9 +9,47 @@ import Link from 'next/link';
 import Image from 'next/image';
 import styles from '@/styles/components/common/Header.module.scss';
 import { useScrollTrigger } from '@/hooks/useScrollTrigger';
+import { useEffect, useState } from 'react';
 import ImageLogo from '@/assets/images/common/kyu-syu-unyu_logo.webp';
 const Header = () => {
   const { ref, isVisible } = useScrollTrigger();
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+  // メニューが開いている間はスクロールを防ぐ
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, [isOpen]);
+
+  // 外側をクリックしたらメニューを閉じる
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (
+        isOpen &&
+        !document.getElementById('headerNav')?.contains(event.target as Node)
+      ) {
+        closeMenu();
+      }
+    };
+
+    document.addEventListener('click', handleOutsideClick);
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, [isOpen]);
+
   return (
     <header className={styles.containerHeader} id="ContainerHeader" ref={ref}>
       <article className={`${isVisible ? styles['is-active'] : ''}`}>
@@ -22,24 +60,53 @@ const Header = () => {
             className={styles.itemLogo}
           />
         </Link>
-        <nav>
-          <Link href="/" className={styles.itemLink}>
+        <nav
+          id="headerNav"
+          className={`${isOpen ? styles['is-open'] : ''} ${
+            !isOpen ? styles.closing : ''
+          }`}
+        >
+          <Link href="/" className={styles.itemLink} onClick={closeMenu}>
             <span>top</span>トップ
           </Link>
-          <Link href="/works/" className={styles.itemLink}>
+          <Link href="/works/" className={styles.itemLink} onClick={closeMenu}>
             <span>works</span>事業内容
           </Link>
-          <Link href="/company/" className={styles.itemLink}>
+          <Link
+            href="/company/"
+            className={styles.itemLink}
+            onClick={closeMenu}
+          >
             <span>company</span>会社概要
           </Link>
-          <Link href="/recruit/" className={styles.itemLink}>
+          <Link
+            href="/recruit/"
+            className={styles.itemLink}
+            onClick={closeMenu}
+          >
             <span>recruit</span>採用情報
           </Link>
-          <Link href="/contact/" className={styles.itemLinkForm}>
+          <Link
+            href="/contact/"
+            className={styles.itemLinkForm}
+            onClick={closeMenu}
+          >
             <span>Contact us</span>ご依頼・お問い合せ
           </Link>
         </nav>
       </article>
+      <button
+        className={`${styles.hamburgerButton} ${
+          isOpen ? styles['is-open'] : ''
+        }`}
+        onClick={toggleMenu}
+        aria-expanded={isOpen}
+        aria-label="メニューを開閉"
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
     </header>
   );
 };
